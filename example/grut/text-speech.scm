a7d3b93dc37636396f98e6008d4140fbff028e81
KOGURO, Naoki <naoki@koguro.net>
2022-01-12 20:11:39 +0900

    Add speech example.

---
+++ example/grut/text-speech.scm
@@ -0,0 +1,21 @@
+(use graviton)
+(use graviton.grut)
+
+(define (main args)
+  (with-window (grut-text-window)
+      (text)
+    (on-jsevent window "keyup" (key)
+      (when (equal? key "Escape")
+        (grv-exit 0)))
+
+    (let1 voice (query-voice :default #t)
+      (format text "Type ~atext message. Press ESC to exit.\n" (if voice #"~(~ voice'lang) " ""))
+      (when voice
+        (format text "\"~a\" voice will be used.\n" (~ voice'name)))
+      (newline text)
+
+      (while #t
+        (let1 msg (read-text/edit text :prompt ">")
+          (unless (= (string-length msg) 0)
+            (format text "Speaking \"~a\".~%" msg)
+            (speak msg)))))))
