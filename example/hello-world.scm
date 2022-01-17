6c0aac6a2699ab75bae59615f0610f2966484650
KOGURO, Naoki <naoki@koguro.net>
2022-01-17 21:07:35 +0900

    Add comments.

--- example/hello-world.scm
+++ example/hello-world.scm
@@ -4,5 +4,7 @@
 (define (main args)
   (with-window (grv-window :body (html:body "Hello,world"))
       ()
+    ;; Waits for a keyup event.
     (jsevent-await window "keyup" ())
+    ;; Closes this window if possible.
     (grv-exit 0)))
