fc4c50cbf41beca8afb59dda214de281af8a64cc
KOGURO, Naoki <naoki@koguro.net>
2022-02-06 13:11:04 +0900

    Update the pakcage spec.

--- package.scm
+++ package.scm
@@ -4,17 +4,18 @@
 
 (define-gauche-package "graviton"
   ;;
-  :version "1.0"
+  :version "0.1"
 
   ;; Description of the package.  The first line is used as a short
   ;; summary.
-  :description "Graphics and sound module."
+  :description "WebUI library for Gauche."
 
   ;; List of dependencies.
   ;; Example:
   ;;     :require (("Gauche" (>= "0.9.5"))  ; requires Gauche 0.9.5 or later
   ;;               ("Gauche-gl" "0.6"))     ; and Gauche-gl 0.6
-  :require ()
+  :require (("Gauche" (>= "0.9.11"))
+            ("Gauche-makiki" (>= "0.2")))
 
   ;; List of providing modules
   ;; NB: This will be recognized >= Gauche 0.9.7.
@@ -25,7 +26,7 @@
   ;; List name and contact info of authors.
   ;; e.g. ("Eva Lu Ator <eval@example.com>"
   ;;       "Alyssa P. Hacker <lisper@example.com>")
-  :authors ("KOGURO, Naoki <naoki@koguro.net>")
+  :authors ("Naoki Koguro <naoki@koguro.net>")
 
   ;; List name and contact info of package maintainers, if they differ
   ;; from authors.
